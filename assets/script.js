5e572cb8d5607f35f42943ed6c1757c6fe3e6500
Adrienne Kecskes <akecs619@yahoo.com>
2023-06-07 18:27:01 -0500

    properly display weather card

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,42 @@
-var forecast = document.getElementById("5-day-forecast")
+var forecast = document.getElementById("forecast-cards")
+var citySearchForm = document.getElementById("city-search")
 
-function featchWeather(){
-    var cityName = document.getElementById("search-city").value
-    var APIKey = "6edb7b4c4ed6f470e643220fdd64ce9b"
-
-fetch(       
-    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=6edb7b4c4ed6f470e643220fdd64ce9b`
-)
-.then(function (response) {
-    return response.json()
-})    
-.then(function (data) {
-    console.log(data)
-    for (var i = 0; i < 5; i++) {
-        var dayContainer = document.createElement("div")
-        var header = document.createElement("h2")
-        var temp = document.createElement("p")
-
-        header.textContent = "Day " + i
-        temp.textContent = data.list[i].main.temp
-        console.log(data.list[i].main.temp)
-
-        dayContainer.setAttribute(
-            "style",
-            "background-color: purple; margin: 10px"
-        )
-            forecast.append(dayContainer)
-            dayContainer.append(header)
-            dayContainer.append(temp)
-        }
-})
+if (citySearchForm) {
+    citySearchForm.addEventListener("submit", fetchWeather)
 }
 
-document.querySelector("button").addEventListener("click", featchWeather)
+function fetchWeather(event) {
+    event.preventDefault()
+    const APIKey = "6edb7b4c4ed6f470e643220fdd64ce9b"
+    const data = new FormData(citySearchForm)
+    const cityName = data.get("city")
+
+    fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${APIKey}`
+    )
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            for (var i = 1; i < 6; i++) {
+                var dayContainer = document.createElement("div")
+                var header = document.createElement("h2")
+                var temp = document.createElement("p")
+
+                header.textContent = "Day " + i
+                temp.textContent = data.list[i].main.temp
+
+                dayContainer.setAttribute(
+                    "class",
+                    "col card bg-info"
+                )
+                forecast.append(dayContainer)
+                dayContainer.append(header)
+                dayContainer.append(temp)
+            }
+        })
+}
+
+
 
 
